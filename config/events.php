d6e104e270e0cb0b739328d7a688767933e0f1e3
Mister-42 <johan@mr42.me>
2026-06-18 07:59:56 +0200

    fix: remove 'events' wrapper key from events config

--- config/events.php
+++ config/events.php
@@ -6,18 +6,14 @@ use YiiRocks\Voyti\Event;
 use YiiRocks\Voyti\Listener;
 
 return [
-    'events' => [
-        Event\AfterLoginEvent::class => [
-            [Listener\PasswordExpirationListener::class, 'onAfterLogin'],
-            [Listener\SessionHistoryListener::class, 'onAfterLogin'],
-        ],
-        Event\AfterRegisterEvent::class => [
-            [Listener\AdminNotificationListener::class, 'onAfterRegister'],
-        ],
-        Event\EmailChangeEvent::class => [
-            [Listener\MailChangeConfirmationListener::class, 'onEmailChange'],
-        ],
-        Event\UserEvent::class => [],
-        Event\FormEvent::class => [],
+    Event\AfterLoginEvent::class => [
+        [Listener\PasswordExpirationListener::class, 'onAfterLogin'],
+        [Listener\SessionHistoryListener::class, 'onAfterLogin'],
+    ],
+    Event\AfterRegisterEvent::class => [
+        [Listener\AdminNotificationListener::class, 'onAfterRegister'],
+    ],
+    Event\EmailChangeEvent::class => [
+        [Listener\MailChangeConfirmationListener::class, 'onEmailChange'],
     ],
 ];
